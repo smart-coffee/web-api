4f77bf5e76a1e369849a403dae2d8f20ee19d7f6
John <jonathan.braat@live.com>
2018-11-17 21:55:57 +0100

    add splash-screen and welcome-screen component

--- code/frontend-angular/src/app/app.module.ts
+++ code/frontend-angular/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { WelcomeScreenComponent } from './components/welcome-screen/welcome-screen.component';
+import { SplashScreenComponent } from './components/splash-screen/splash-screen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeScreenComponent,
+    SplashScreenComponent
   ],
   imports: [
     BrowserModule
