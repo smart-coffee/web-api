a4867b54e48f179247b92caba26cd026e5ff4e67
Tobias Blaufuß <t.blaufuss@gmx.de>
2018-11-27 13:16:38 +0100

    [CHANGE] Replaced confusing backref fields with back_populates

--- code/webapi/src/models/__init__.py
+++ code/webapi/src/models/__init__.py
@@ -12,13 +12,16 @@ class User(DB.Model):
 	password = DB.Column(DB.String(150), nullable=False)
 	email = DB.Column(DB.String(50), nullable=False)
 	
-	# Relationships: 1..n
-	profiles = DB.relationship('Profile', backref='user' )
-	jobs = DB.relationship('Job', backref='user' )
-	
-	# Backref
+	# Foreign Keys
 	role_id_fk = DB.Column(DB.Integer, DB.ForeignKey('role.id', name='fk_user_role'), nullable=False)
 	
+	# Relationships: 1 .. n
+	profiles = DB.relationship('Profile', back_populates='user')
+	jobs = DB.relationship('Job', back_populates='user')
+	
+	# Relationships: n .. 1
+	role = DB.relationship('Role', foreign_keys=[role_id_fk], back_populates='users')
+
 	def get_id(self):
 		return self.id
 		
@@ -30,8 +33,8 @@ class Role(DB.Model):
 	# Attributes
 	name = DB.Column(DB.String(30), unique=True, nullable=False)
 	
-	# Relationships: 1..n
-	users = DB.relationship('User', backref='role' )
+	# Relationships: 1 .. n
+	users = DB.relationship('User', back_populates='role')
 	
 	def get_id(self):
 		return self.id
@@ -45,10 +48,13 @@ class Profile(DB.Model):
 	name = DB.Column(DB.String(30), unique=True, nullable=False)
 	coffee_strength_in_percent = DB.Column(DB.Integer, nullable=False)
 	water_in_percent = DB.Column(DB.Integer, nullable=False)
+
+	# Foreign Keys
+	user_id_fk = DB.Column(DB.Integer, DB.ForeignKey('user.id', name='fk_profile_user'), nullable=False)
 	
-	# Backref
-	user_id_fk = DB.Column(DB.Integer, DB.ForeignKey('user.id', name='fk_profile_user'))
-	
+	# Relationships: n .. 1
+	user = DB.relationship('User', foreign_keys=[user_id_fk], back_populates='profiles')
+
 	def get_id(self):
 		return self.id
 		
@@ -60,8 +66,8 @@ class CoffeeBrand(DB.Model):
 	# Attributes
 	name = DB.Column(DB.String(30), unique=True, nullable=False)
 	
-	# Relationships: 1..n
-	products = DB.relationship('CoffeeProduct', backref='coffee_brand' )
+	# Relationships: 1 .. n
+	products = DB.relationship('CoffeeProduct', back_populates='coffee_brand')
 	
 	def get_id(self):
 		return self.id
@@ -74,8 +80,8 @@ class CoffeeType(DB.Model):
 	# Attributes
 	name = DB.Column(DB.String(30), unique=True, nullable=False)
 	
-	# Relationships: 1..n
-	products = DB.relationship('CoffeeProduct', backref='coffee_type' )
+	# Relationships: 1 .. n
+	products = DB.relationship('CoffeeProduct', back_populates='coffee_type')
 	
 	def get_id(self):
 		return self.id
@@ -87,14 +93,18 @@ class CoffeeProduct(DB.Model):
 	
 	# Attributes
 	name = DB.Column(DB.String(50), unique=True, nullable=False)
-	
-	# Relationships: 1..n
-	jobs = DB.relationship('Job', backref='coffee_product' )
-	
-	# Backref
+
+	# Foreign Keys
 	coffee_type_id_fk = DB.Column(DB.Integer, DB.ForeignKey('coffeetype.id', name='fk_coffeeproduct_coffee_type'))
 	coffee_brand_id_fk = DB.Column(DB.Integer, DB.ForeignKey('coffeebrand.id', name='fk_coffeeproduct_coffee_brand'))
 	
+	# Relationships: 1 .. n
+	jobs = DB.relationship('Job', back_populates='coffee_product')
+	
+	# Relationships: n .. 1
+	coffee_type = DB.relationship('CoffeeType', foreign_keys=[coffee_type_id_fk], back_populates='products')
+	coffee_brand = DB.relationship('CoffeeBrand', foreign_keys=[coffee_brand_id_fk], back_populates='products')
+
 	def get_id(self):
 		return self.id
 		
@@ -106,8 +116,8 @@ class CoffeeMachine(DB.Model):
 	# Attributes
 	name = DB.Column(DB.String(50), unique=True, nullable=False)
 	
-	# Relationships: 1..n
-	jobs = DB.relationship('Job', backref='coffee_machine' )
+	# Relationships: 1 .. n
+	jobs = DB.relationship('Job', back_populates='coffee_machine')
 	
 	def get_id(self):
 		return self.id
@@ -125,10 +135,15 @@ class Job(DB.Model):
 	price = DB.Column(DB.Integer, nullable=False)
 	doeses = DB.Column(DB.Integer, nullable=False)
 	
-	# Backref
+	# Foreign Keys
 	coffee_machine_id_fk = DB.Column(DB.Integer, DB.ForeignKey('coffeemachine.id', name='fk_job_coffee_machine'), nullable=False)
 	coffee_product_id_fk = DB.Column(DB.Integer, DB.ForeignKey('coffeeproduct.id', name='fk_job_coffee_product'), nullable=False)
 	user_id_fk = DB.Column(DB.Integer, DB.ForeignKey('user.id', name='fk_job_user'), nullable=False)
 	
+	# Relationships: n .. 1
+	user = DB.relationship('User', foreign_keys=[user_id_fk], back_populates='jobs')
+	coffee_machine = DB.relationship('CoffeeMachine', foreign_keys=[coffee_machine_id_fk], back_populates='jobs')
+	coffee_product = DB.relationship('CoffeeProduct', foreign_keys=[coffee_product_id_fk], back_populates='jobs')
+
 	def get_id(self):
 		return self.id
