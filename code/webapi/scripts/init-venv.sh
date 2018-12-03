953f900a2a8f310f7dec402e74a6774bc372dbd9
Tobias Blaufuß <t.blaufuss@gmx.de>
2018-12-03 16:48:18 +0100

    [UPDATE] Scripts

--- code/webapi/scripts/init-venv.sh
+++ code/webapi/scripts/init-venv.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
-python -m venv venv
+python3.6 -m venv venv
+if [[ "$?" = "127" ]]; then
+    echo "Trying python3.7"
+    python3.7 -m venv venv
+fi
+if [[ "$?" = "127" ]]; then
+    echo "python3.6 or python3.7 is not installed."
+    exit 1
+fi
+
 source venv/bin/activate
 pip install --upgrade pip setuptools
 pip install -r requirements.txt
