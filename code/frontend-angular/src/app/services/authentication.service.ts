9aee54b2cb793f1305ec7c2551a54f802a7312d5
John <jonathan.braat@live.com>
2018-12-04 21:48:40 +0100

    [REFACTOR] authentication service

--- code/frontend-angular/src/app/services/authentication.service.ts
+++ code/frontend-angular/src/app/services/authentication.service.ts
@@ -27,7 +27,6 @@ export class AuthenticationService {
       .pipe(map(user => {
         // login successful if there's a jwt token in the response
         if (user && user.token) {
-          // store user details and jwt token in local storage to keep user logged in between page refreshes
           localStorage.setItem('currentUser', JSON.stringify(user));
           this.currentUserSubject.next(user);
         }
