5aacf0118e58b9d175608e74d106a3a1c87f6ca2
John <jonathan.braat@live.com>
2018-12-04 21:47:30 +0100

    [REFACTOR] remove unused imports from home component

--- code/frontend-angular/src/app/components/home/home.component.ts
+++ code/frontend-angular/src/app/components/home/home.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { CoffeeMachine } from '../../shared/models/coffee-machine';
 
 @Component({
   selector: 'app-home',
