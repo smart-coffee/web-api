ac934fbad692f5cdc48b1cea8ac5b4e9dc54f2fe
Tobias Blaufuß <t.blaufuss@gmx.de>
2018-12-11 18:15:00 +0100

    [FIX] Removed 'user_id' from /users/{public_id}/profiles POST request

--- code/webapi/src/controllers/request_model/__init__.py
+++ code/webapi/src/controllers/request_model/__init__.py
@@ -204,7 +204,6 @@ def get_create_user_profile_request_fields():
     file: /controllers/request_model/create_user_profile_request.yml
     """
     _create_profile_fields = {
-        'user_id': fields.String,
         'name': fields.String,
         'coffee_strength_in_percent': fields.Integer,
         'water_in_percent': fields.Integer
