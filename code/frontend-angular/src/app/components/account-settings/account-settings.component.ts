e51e79f406e8e084106659f95ba10ed181589112
Jonathan Braat <jonathan.braat@live.com>
2018-12-11 17:46:31 +0100

    [ADD] account settings component

--- code/frontend-angular/src/app/components/account-settings/account-settings.component.ts
+++ code/frontend-angular/src/app/components/account-settings/account-settings.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ITextInputObject } from '../../shared/interfaces/form-input-objects';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-account-settings',
@@ -7,9 +11,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountSettingsComponent implements OnInit {
 
-  constructor() { }
+  username: string;
+  oldPassword: string;
+  newPassword: string;
+  newPasswordConfirm: string;
+  email: string;
+
+  constructor(private router: Router,
+              private location: Location,
+              private userService: UserService) { }
 
   ngOnInit() {
+    this.username = '';
+    this.oldPassword = '';
+    this.newPassword = '';
+    this.newPasswordConfirm = '';
+    this.email = '';
+  }
+
+  onSwipeRight () {
+    this.goBack();
+  }
+
+  goBack() {
+    this.location.back();
+  }
+
+  onTyped (inputInfo: ITextInputObject ) {
+    switch (inputInfo.fieldId) {
+      case 'username': this.username = inputInfo.value; break;
+      case 'oldPassword': this.oldPassword = inputInfo.value; break;
+      case 'newPassword': this.newPassword = inputInfo.value; break;
+      case 'newPasswordConfirm': this.newPasswordConfirm = inputInfo.value; break;
+      case 'email': this.email = inputInfo.value; break;
+      default: console.error('something broke in the sign in input distinction');
+    }
+  }
+
+  editUser() {
+    if (this.newPassword === this.newPasswordConfirm) {
+      const tmpUser = {
+        name: this.username,
+        old_password: this.oldPassword,
+        new_password: this.newPassword,
+        email: this.email
+      };
+      this.userService.editUserDetails(tmpUser)
+        .subscribe(user => {
+          console.log(user);
+        });
+    }
   }
 
 }
