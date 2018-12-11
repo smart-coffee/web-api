d28e939f1bcaad86d67d6d40a0ef5d414d9ac8c8
Jonathan Braat <jonathan.braat@live.com>
2018-12-11 16:55:22 +0100

    [ADD] get current user, post new user, edit user details in user service

--- code/frontend-angular/src/app/services/user.service.ts
+++ code/frontend-angular/src/app/services/user.service.ts
@@ -14,6 +14,13 @@ export class UserService {
 
   constructor(private http: HttpClient) { }
 
+  getCurrentUser(): Observable<any> {
+    return this.http.get<any>(`${environment.webApiUrl}/users/current`)
+      .pipe(
+        catchError(this._error.handleError<any>(`getCurrentUser`))
+      );
+  }
+
   getCoffeeProfiles(): Observable<any> {
     return this.http.get<any>(`${environment.webApiUrl}/users/current/profiles`)
       .pipe(
@@ -25,6 +32,20 @@ export class UserService {
     return this.http.post<any>(`${environment.webApiUrl}/users/current/profiles`, newProfile)
       .pipe(
         catchError(this._error.handleError<any>(`postNewCoffeeProfile`, newProfile))
+      );
+  }
+
+  postNewUser(newUser: any): Observable<any> {
+    return this.http.post<any>(`${environment.webApiUrl}/public/users`, newUser)
+      .pipe(
+        catchError(this._error.handleError<any>(`postNewUser`, newUser))
+      );
+  }
+
+  editUserDetails(newDetails: any): Observable<any> {
+    return this.http.put<any>(`${environment.webApiUrl}/users/current`, newDetails)
+      .pipe(
+        catchError(this._error.handleError<any>(`editUserDetails`, newDetails))
       );
   }
 
