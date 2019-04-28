787196565e85f52091f90ae4be8f264f4c8dee98
Tobias Blaufuß <t.blaufuss@gmx.de>
2019-04-28 23:37:00 +0200

    [FIX] pool recycle issue

--- src/config/production.py
+++ src/config/production.py
@@ -5,3 +5,10 @@ DEBUG = False
 PROPAGATE_EXCEPTIONS = True
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_DATABASE_URI = get_connection_uri_from_env()
+
+# https://flask-sqlalchemy.palletsprojects.com/en/2.x/config/#configuration-keys
+# https://docs.sqlalchemy.org/en/13/core/engines.html#engine-creation-api
+# https://flask-sqlalchemy.palletsprojects.com/en/2.x/api/#flask_sqlalchemy.SQLAlchemy.create_engine
+SQLALCHEMY_ENGINE_OPTIONS = {
+    "pool_recycle" : 500
+}
