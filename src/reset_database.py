a9a803be4d8ae75fd6ee372790895b17d7269124
Tobias Blaufuß <t.blaufuss@gmx.de>
2019-04-29 22:37:28 +0200

    [ADD] GET /coffee/machines/{id}/jobs and GET /coffee/machines/{id}/jobs/count, closes #2

--- src/reset_database.py
+++ src/reset_database.py
@@ -13,6 +13,7 @@ def reset_database():
 
     user = User()
     user.public_id = 'd38924fb-9417-4a50-b715-01f805c28063'
+    # password
     user.password = 'sha256$PaWEzGH9$7ee9e7ea744fb7c10d9332b21d8d0c0e611aa37dac9e2b493a658aaa07168b25'
     user.name = 'admin'
     user.email = 'some.email@domain.tld'
